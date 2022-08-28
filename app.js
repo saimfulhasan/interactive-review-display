98eb7a9fd5c306e64cd3bb1a535a8f8d8de64c57
saimfulhasan <saifulsaimshamim@gmail.com>
2022-08-28 09:11:07 +0600

    image added

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ const reviewContainer = document.getElementById('review-container')
 
 const reviews = [
     {
-        img: 'https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bWFufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
-        alt: 'Profile picture of Indvah Almay',
+        img: 'https://images.unsplash.com/photo-1457732815361-daa98277e9c8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8d29tYW58ZW58MHwwfDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
         text: 'I really enjoyed this course! It was fun and interactive as well as educational. I learnt a lot.',
         name: 'Indvah Almay'
     },
@@ -14,8 +13,7 @@ const reviews = [
         name: 'Andrew Ligo'
     },
     {
-        img: 'https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bWFufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
-        alt: 'Profile picture of Cira Enaj',
+        img: 'https://images.unsplash.com/photo-1484399172022-72a90b12e3c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Z2lybHxlbnwwfDB8MHx8&auto=format&fit=crop&w=500&q=60',
         text: 'I manage to do this course in my spare time after hours and have only positive things to say.',
         name: 'Cira Enaj'
     }
